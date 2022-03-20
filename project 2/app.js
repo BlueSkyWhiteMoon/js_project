299dea2e52553a122711abadc0e520cc3ff2fa96
BlueSkyWhiteMoon <Mutaliev96@outlook.com>
2022-03-20 20:45:53 +0100

    creating array of objects

---
+++ project 2/app.js
@@ -0,0 +1,26 @@
+let data = [
+    {
+        name: 'Til',
+        age: '30'
+    },
+    {
+        name: 'Sam',
+        age: '31'
+    },
+    {
+        name: 'Jaja',
+        age: '32'
+    },
+    {
+        name: 'lalal',
+        age: '33'
+    },
+    {
+        name: 'banana',
+        age: '34'
+    },
+    {
+        name: 'ihata',
+        age: '35'
+    },
+]
