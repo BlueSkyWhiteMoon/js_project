9469d2fd9e4928939e3821c0a9a828015e8ef2e0
BlueSkyWhiteMoon <Mutaliev96@outlook.com>
2022-03-20 21:09:21 +0100

    adding query Selector to reference HTML Element, mapping through array and displaying values

--- project 2/app.js
+++ project 2/app.js
@@ -22,5 +22,14 @@ let data = [
     {
         name: 'ihata',
         age: '35'
-    },
-]
+    }
+];
+
+const info = document.querySelector('#info')
+
+let details = data.map(function(item) {
+    return '<div>' + item.name + ' ' + 'is ' + item.age + ' years old.' + '</div>';
+
+});
+
+info.innerHTML = details.join('\n');
